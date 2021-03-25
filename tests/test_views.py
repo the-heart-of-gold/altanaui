f62bb362203f5b2395639b95ccc42fbb8c01706a
the-heart-of-gold <dave.dyke@nssif.gov.uk>
2021-03-25 19:52:14 +0000

    Cookiecutter pyramid template.

---
+++ tests/test_views.py
@@ -0,0 +1,13 @@
+from altanaui.views.default import my_view
+from altanaui.views.notfound import notfound_view
+
+
+def test_my_view(app_request):
+    info = my_view(app_request)
+    assert app_request.response.status_int == 200
+    assert info['project'] == 'altanui'
+
+def test_notfound_view(app_request):
+    info = notfound_view(app_request)
+    assert app_request.response.status_int == 404
+    assert info == {}
